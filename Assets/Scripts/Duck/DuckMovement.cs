6735ca0c175d22bfb284c0d67178f7065988d26e
Ivan Frolov <mail.f1van@gmail.com>
2023-07-07 23:59:13 +0300

    prototyping duck movement

--- Assets/Scripts/Duck/DuckMovement.cs
+++ Assets/Scripts/Duck/DuckMovement.cs
@@ -7,10 +7,10 @@ public class DuckMovement : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _rigidbody;
     [SerializeField] private float _horizontalSpeed;
-    [SerializeField] private float _gravity;
     [SerializeField] private float _swingForce;
+    [SerializeField] private float _maxVerticalVelocity;
     
-    private Vector2 _direction;
+    private int _direction;
     
     private void Start()
     {
@@ -19,32 +19,31 @@ public class DuckMovement : MonoBehaviour
 
     public void Initialize()
     {
-        _direction = Vector2.right;
-        Physics.gravity = new Vector3(0, _gravity, 0);
-    }
-
-    private void Update()
-    {
-        MoveHorizontaly();
+        _direction = 1;
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
-            Swing();
+              Swing();
+
+        MoveHorizontaly();
     }
 
     private void MoveHorizontaly()
     {
-        if (Input.GetAxis("Horizontal") != 0)
-            _direction = new Vector2(Input.GetAxis("Horizontal"), 0);
-        
-        _rigidbody.MovePosition(_direction * _horizontalSpeed);
+        var direction = Input.GetAxis("Horizontal");
+        if (direction != 0)
+            _direction = direction > 0 ? 1 : -1;
+
+        _rigidbody.velocity = new Vector2(_direction * _horizontalSpeed, _rigidbody.velocity.y);
     }
 
     private void Swing()
     {
         _rigidbody.AddForce(Vector2.up * _swingForce, ForceMode2D.Impulse);
+        float verticalVelocity = Mathf.Clamp(_rigidbody.velocity.y, -_maxVerticalVelocity, _maxVerticalVelocity);
+        _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, verticalVelocity);
     }
 }
