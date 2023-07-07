f8ed7d6767702a5347301e1a6e8b2500ffb05310
Ivan Frolov <mail.f1van@gmail.com>
2023-07-07 23:24:48 +0300

    Added Startup with gameloop

---
+++ Assets/Scripts/Duck/DuckContainer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class DuckContainer : MonoBehaviour
+{
+    [SerializeField] private DuckMovement _duckMovement;
+    private InputService _inputService;
+
+
+    public void Initialize(InputService inputService)
+    {
+        _inputService = inputService;
+        _duckMovement.Initialize();
+    }
+}
---
+++ Assets/Scripts/Infrastructure/GameLoop.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class GameLoop : MonoBehaviour
+{
+    [SerializeField] private DuckContainer _duckPrefab;
+    
+    private InputService _inputService;
+    private DuckContainer _duckContainer;
+
+    public void Initialize(InputService inputService)
+    {
+        
+    }
+
+    public void Begin()
+    {
+        
+    }
+}
