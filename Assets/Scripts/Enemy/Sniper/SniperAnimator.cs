82918a95034bc89b3a10e4a853f40e72b99910bf
Farid Yusubov <Blackwazz7@gmail.com>
2023-07-09 17:41:25 +0500

    Merge branch 'Farid'

--- Assets/Scripts/Enemy/Sniper/SniperAnimator.cs
+++ Assets/Scripts/Enemy/Sniper/SniperAnimator.cs
@@ -5,31 +5,30 @@ namespace Enemy
     /// <summary>
     /// Класс содержит методы, изменяющие анимацию снайпера
     /// </summary>
-    public class SniperAnimator : MonoBehaviour
+    public class SniperAnimator : EnemyAnimator
     {
-        [SerializeField] private Animator _animator;
-     
+        [SerializeField] private Animator _sniperAnimator;
+
         private readonly int _isWalking = Animator.StringToHash("IsWalking");
         private readonly int _isAiming = Animator.StringToHash("ISAiming");
         private readonly int _shoot = Animator.StringToHash("Shoot");
 
         public void StartWalking()
         {
-            _animator.SetBool(_isAiming, false);
-            _animator.SetBool(_isWalking, true);
+            _sniperAnimator.SetBool(_isAiming, false);
+            _sniperAnimator.SetBool(_isWalking, true);
         }
 
 
         public void MountForAim()
         {
-            _animator.SetBool(_isWalking, false);
-            _animator.SetBool(_isAiming, true);
+            _sniperAnimator.SetBool(_isWalking, false);
+            _sniperAnimator.SetBool(_isAiming, true);
         }
 
         public void ExpressShoot()
         {
-            _animator.SetTrigger(_shoot);
+            _sniperAnimator.SetTrigger(_shoot);
         }
-
     }
 }
